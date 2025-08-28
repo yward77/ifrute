6dcff6b35de7f0885c31eaa7c4ce1aeb9e624940
yazan ward <yazanward8@gmail.com>
2025-08-28 22:21:35 +0300

    Add files via upload

---
+++ yy.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+  const menu = document.getElementById('sideMenu');
+  menu.style.right = (menu.style.right === '0px') ? '-250px' : '0px';
+}
+function toggleMenu() {
+    const sideMenu = document.getElementById('sideMenu');
+    if(sideMenu.style.right === "0px") {
+        sideMenu.style.right = "-150px";
+    } else {
+        sideMenu.style.right = "0px";
+    }
+}  
